01234f5dabb47edc36251a0f508dd79e5c8da66e
Minh Khoa Bui <mkhoabui@gmail.com>
2021-11-02 20:58:40 -0400

    fix a bug where quotes can come from same movie

--- src/index.js
+++ src/index.js
@@ -4,7 +4,18 @@ import Header from './components/Header/Header';
 import Form from './components/Form/Form';
 
 const movieQuote = require("popular-movie-quotes");
-const quotes = movieQuote.getSomeRandom(4);
+const numQuotes = 4;
+const quotes = movieQuote.getSomeRandom(1);
+const movies = [quotes[0].movie];
+
+for (let i = 0; i < numQuotes - 1; i++) {
+  let nextQuote = movieQuote.getSomeRandom(1);
+  while (movies.includes(nextQuote[0].movie)) {
+    nextQuote = movieQuote.getSomeRandom(1);
+  }
+  quotes.push(nextQuote[0]);
+  movies.push(nextQuote[0].movie);
+}
 
 ReactDOM.render(
   <React.StrictMode>
