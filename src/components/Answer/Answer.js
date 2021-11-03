7d4fba4cef09c5278d878ca8c6cdd290543f2bcf
Minh Khoa Bui <mkhoabui@gmail.com>
2021-11-02 21:59:49 -0400

    add submit button that is hidden by default

--- src/components/Answer/Answer.js
+++ src/components/Answer/Answer.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 function Answer(props) {
+    const handleChange = () => {
+        props.onChange();
+    };
+
     return (
         <div>
-            <input type="radio" name="movie" id={props.id} />
+            <input type="radio" name="movie" id={props.id} onChange={handleChange} />
             <label for={props.id}>{props.movie}</label>
         </div>
     );
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,18 +3,45 @@ import Answer from '../Answer/Answer';
 import Question from '../Question/Question';
 import { nanoid } from 'nanoid';
 
-function Form(props) {
-    const answerList = props.quotes.map(quote => (
-        <Answer movie={quote.movie} id={"answer-" + nanoid()} />
-    ));
-    const randomQuote = props.quotes[Math.floor(Math.random() * 4)];
+// function Form(props) {
+//     const answerList = props.quotes.map(quote => (
+//         <Answer movie={quote.movie} id={"answer-" + nanoid()} />
+//     ));
+//     const randomQuote = props.quotes[Math.floor(Math.random() * 4)];
 
-    return (
-        <form>
-            <Question randomQuote={randomQuote.quote} />
-            {answerList}
-        </form>
-    );
+//     return (
+//         <form>
+//             <Question randomQuote={randomQuote.quote} />
+//             {answerList}
+//             <button>Submit Answer</button>
+//         </form>
+//     );
+// }
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { shouldHide: true };
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange() {
+        this.setState({ shouldHide: false });
+    }
+
+    render() {
+        const answerList = this.props.quotes.map(quote => (
+            <Answer movie={quote.movie} id={"answer-" + nanoid()} onChange={this.onChange}  />
+        ));
+
+        return (
+            <form>
+                <Question randomQuote={this.props.randomQuote.quote} />
+                {answerList}
+                <button className={this.state.shouldHide ? 'hidden' : undefined}>Submit Answer</button>
+            </form>
+        );
+    }
 }
 
 export default Form;
