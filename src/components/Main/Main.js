62213607ba1d9aae95db044587ebe7f306a6baa3
Minh Khoa Bui <mkhoabui@gmail.com>
2021-11-05 22:31:52 -0400

    refactor getting quotes

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,23 +2,28 @@ import React from "react";
 import Form from "../Form/Form";
 import Score from "../Score/Score";
 
+const getQuotes = () => {
+    const movieQuote = require("popular-movie-quotes");
+    const numQuotes = 4;
+    const quotes = movieQuote.getSomeRandom(1);
+    const movies = [quotes[0].movie];
+    //prevent quotes from same movie
+    for (let i = 0; i < numQuotes - 1; i++) {
+        let nextQuote = movieQuote.getSomeRandom(1);
+        while (movies.includes(nextQuote[0].movie)) {
+            nextQuote = movieQuote.getSomeRandom(1);
+        }
+        quotes.push(nextQuote[0]);
+        movies.push(nextQuote[0].movie);
+    }
+    return quotes;
+};
+
 class Main extends React.Component {
     constructor(props) {
         super(props);
 
-        const movieQuote = require("popular-movie-quotes");
-        const numQuotes = 4;
-        const quotes = movieQuote.getSomeRandom(1);
-        const movies = [quotes[0].movie];
-        //prevent quotes from same movie
-        for (let i = 0; i < numQuotes - 1; i++) {
-            let nextQuote = movieQuote.getSomeRandom(1);
-            while (movies.includes(nextQuote[0].movie)) {
-                nextQuote = movieQuote.getSomeRandom(1);
-            }
-            quotes.push(nextQuote[0]);
-            movies.push(nextQuote[0].movie);
-        }
+        const quotes = getQuotes();
         const randomQuote = quotes[Math.floor(Math.random() * 4)];
 
         this.state = {
@@ -37,19 +42,7 @@ class Main extends React.Component {
     }
 
     incrementCountQuestion() {
-        const movieQuote = require("popular-movie-quotes");
-        const numQuotes = 4;
-        const quotes = movieQuote.getSomeRandom(1);
-        const movies = [quotes[0].movie];
-        //prevent quotes from same movie
-        for (let i = 0; i < numQuotes - 1; i++) {
-            let nextQuote = movieQuote.getSomeRandom(1);
-            while (movies.includes(nextQuote[0].movie)) {
-                nextQuote = movieQuote.getSomeRandom(1);
-            }
-            quotes.push(nextQuote[0]);
-            movies.push(nextQuote[0].movie);
-        }
+        const quotes = getQuotes();
         const randomQuote = quotes[Math.floor(Math.random() * 4)];
 
         this.setState({
