0bb1521933e0544a55ef08d3722f0d8c3536733d
Minh Khoa Bui <mkhoabui@gmail.com>
2021-11-14 14:34:04 -0500

    add some bootstrap classes

--- src/components/Reward/Reward.js
+++ src/components/Reward/Reward.js
@@ -2,7 +2,7 @@ function Reward(props) {
     return (
         <div className={props.shouldHide ? 'hidden' : undefined}>
             <p>Congratulations on reaching {props.countCorrect} correct answers. Here's a reward:</p>
-            <img src={props.rewardImageSrc} alt="reward" />
+            <img class="img-fluid" src={props.rewardImageSrc} alt="reward" />
         </div>
     );
 }
--- src/functions.js
+++ src/functions.js
@@ -3,11 +3,15 @@ export const getQuotes = () => {
     const numQuotes = 4;
     const quotes = movieQuote.getSomeRandom(1);
     const movies = [quotes[0].movie];
+    const punctuations = ['?', '.', '!'];
     //prevent quotes from same movie
     for (let i = 0; i < numQuotes - 1; i++) {
         let nextQuote = movieQuote.getSomeRandom(1);
         while (movies.includes(nextQuote[0].movie)) {
             nextQuote = movieQuote.getSomeRandom(1);
+        }
+        if (!punctuations.includes(nextQuote[0].quote.slice(-1))) {
+            nextQuote[0].quote += '.';
         }
         quotes.push(nextQuote[0]);
         movies.push(nextQuote[0].movie);
